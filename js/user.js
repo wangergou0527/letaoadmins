eb717300c512da8318bed130e26a4d4bf8b407be
shazi <138199527@qq.com>
2018-06-20 11:59:54 +0800

    第二次上传

--- js/user.js
+++ js/user.js
@@ -1,3 +1,16 @@
+$.ajax({
+	type: 'get',
+	async: false,
+	url: `${APP.baseUrl}/employee/checkRootLogin`,
+	success: function (response) {
+		if (response.error) {
+			location.href = 'login.html'
+		}
+	}
+})
+
+
+
 $(function () {
 	$.ajax({
 		type: 'get',
@@ -11,5 +24,28 @@ $(function () {
 			var html = template('userTemp', response);
 			$('#userBox').html(html);
 		}
+	})
+
+	$('#userBox').on('click', '.changeStatus', function () {
+		
+		var id = $(this).attr('data-user-id');
+
+		var isdelete = $(this).attr('data-user-isdelete');
+
+		$.ajax({
+			type: 'post',
+			url: `${APP.baseUrl}/user/updateUser`,
+			data: {
+				id: id,
+				isdelete: isdelete == 1 ? 0 : 1
+			},
+			success: function (response) {
+				if (response.success) {
+					location.reload();
+				} else {
+					alert(response.message);
+				}
+			}
+		})
 	})
 })
